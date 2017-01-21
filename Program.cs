b17de37fe43d772254536101c094ceb16a075814
CORP\Neeharika.Pathuri <neehu395@gmail.com>
2017-01-21 20:20:28 +0530

    no message

--- Program.cs
+++ Program.cs
@@ -11,56 +11,56 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             int[] input = new int[10];
-                for (int index = 1; index <= 10; index += 2)
-                {
+            for (int index = 1; index <= 10; index += 2)
+            {
 
-                    input[index] = index;
-                    index++;
-                }
+                input[index] = index;
+                index++;
+            }
 
             int[] output = RemoveAlternateElements(input);
             Console.WriteLine("1st");
-            int[] finalanswer=new int [output.Length];
+            int[] finalanswer = new int[output.Length];
             Console.WriteLine("2nd");
 
-                while (finalanswer.Length <1)
-                {
-                    Console.WriteLine("2nd");
-                    finalanswer = RemoveAlternateElements(output);
-                }
-
-                foreach (int v in finalanswer)
-                {
-                    Console.WriteLine(v);
-                }
-                Console.ReadLine();
+            while (finalanswer.Length < 1)
+            {
+                Console.WriteLine("2nd");
+                finalanswer = RemoveAlternateElements(output);
             }
+
+            foreach (int v in finalanswer)
+            {
+                Console.WriteLine(v);
+            }
+            Console.ReadLine();
+        }
 
 
         static int[] RemoveAlternateElements(int[] input)
         {
-              int temp, oindex = 1;
-            
-                if (input.Length % 2 == 0)
-                {
-                    temp = (input.Length / 2);
-                }
-                else
-                {
-                    temp = (input.Length / 2) + 1;
-                }
+            int temp, oindex = 1;
+
+            if (input.Length % 2 == 0)
+            {
+                temp = (input.Length / 2);
+            }
+            else
+            {
+                temp = (input.Length / 2) + 1;
+            }
 
             int[] output = new int[temp];
 
-               for (int index = 0; index<=temp; index =index+1)
-               {
-                
-                    
+            for (int index = 0; index <= temp; index = index + 1)
+            {
+
+
                 output[oindex] = input[index];
-                
+
 
             }
-           
+
             return output;
             //determine the size of o/p array//
             //define an array=output array;
